b6d38b9d9b512423ae0267b26b1ce1b88cac9479
Chris Phillips <chrisphillers@gmail.com>
2018-09-28 14:05:10 +0100

    First commit, Films displaying, Search/Fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,74 @@
 import React from 'react';
+import Films from './Films';
+import Search from './Search';
 
 class App extends React.Component {
   constructor(){
     super();
+
+    this.state = {films: []};
+
+    this.fetchIMDB = this.fetchIMDB.bind(this);
+    // this.filmSearch = this.filmSearch.bind(this);
+    // this.IMDBData= this.IMDBData.bind(this);
+    this.notFound= this.notFound.bind(this);
+    // this.searchText = this.searchText.bind(this);
+    this.receiveSearch = this.receiveSearch.bind(this);
   }
+
+  componentDidMount(){
+    this.receiveSearch('Interstellar');
+  }
+
+  notFound(){
+    console.log("notFound");
+  }
+
+  receiveSearch(filmSearchQuery){
+    const searchURL = `https://www.omdbapi.com/?s=${filmSearchQuery}&apikey=73071eff`;
+    this.fetchIMDB(searchURL);
+  }
+
+
+  
+  // Initial Search Fetch
+  fetchIMDB(searchURL){
+    fetch(searchURL) 
+    .then(function(response) {
+        
+        return response.json();
+    })
+    .then(films=> {
+      if(films.Response === "False") {
+        this.notFound()
+        console.log("WHOOPS!");
+      } else {
+        console.log(films)
+        this.setState({films: films.Search})}
+    }
+    );
+  }
+
+// searchText(){
+//   console.log("SearchQuery")
+// }
+  
+  
 
   render(){
     return (
       <div>
-        React cinema app
+        <Search receiveSearch={this.receiveSearch}/>
+        <Films films={this.state.films}/>
       </div>
-    )
+    );
   }
+
+
+
+
 }
 
 export default App;
+
+
