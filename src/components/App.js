44ecd28ba17072e16f7fd0ce5010f3fb5d8ff0e7
Joe Lamb <joe@elevenplustwo.co.uk>
2018-09-28 13:02:58 +0100

    display search results

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
+import Header from './Header';
+import Search from './Search';
+import SearchResults from './SearchResults';
 
 class App extends React.Component {
-  constructor(){
-    super();
-  }
+    constructor() {
+        super();
 
-  render(){
-    return (
-      <div>
-        React cinema app
-      </div>
-    )
-  }
+        this.state = {
+            films: [],
+            totalFilms: 0
+        }
+    }
+
+    searchByTitle() {
+        fetch("http://www.omdbapi.com/?apikey=507b4100&type=movie&s=Titanic")
+            .then(response => response.json())
+            .then(body => {
+                this.setState({
+                    films: body.Search,
+                    totalFilms: body.totalResults
+                }, () => console.log(this.state))
+            })
+            .catch(error => {
+                alert(error);
+            })
+    }
+
+    componentWillMount() {
+        this.searchByTitle();
+    }
+
+
+
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Search />
+                <SearchResults films={this.state.films} />
+            </div>
+        )
+    }
 }
 
 export default App;
