9974e03d257c45826a9c894fba712ab1e576d726
Joe Lamb <joe@elevenplustwo.co.uk>
2018-09-28 21:51:57 +0100

    page advance and reverse

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,6 +7,20 @@ class Pagination extends React.Component {
         this.state = {
             currentPage: 1
         }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        if (e.target.className.includes("btn__next")) {
+            this.setState({
+                currentPage: this.state.currentPage + 1
+            });
+        } else if (e.target.className.includes("btn__prev")) {
+            this.setState({
+                currentPage: this.state.currentPage - 1
+            });
+        };
     }
 
     render() {
@@ -14,7 +28,7 @@ class Pagination extends React.Component {
         const totalPages = Math.ceil(this.props.totalFilms / this.props.filmsPerPage);
 
         return (
-            <nav id="page-nav">
+            <nav id="page-nav" onClick={this.handleClick}>
                 <button className="btn btn__prev">&larr;</button>
                 <p className="page-num">Page <span className="page-current">{this.state.currentPage}</span> of <span className="page-total">{totalPages}</span></p>
                 <button className="btn btn__next">&rarr;</button>
