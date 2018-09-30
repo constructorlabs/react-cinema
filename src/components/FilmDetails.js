56f85f02523742631989146824534305a4da115e
Joe Lamb <joe@elevenplustwo.co.uk>
2018-09-30 13:38:28 +0100

    mobile film details styling

--- src/components/FilmDetails.js
+++ src/components/FilmDetails.js
@@ -26,22 +26,24 @@ class FilmDetails extends React.Component {
         );
 
         return (
-            <div className="film-details__wrapper" >
-                <h3 className="film-details__title">{this.props.filmDetails.Title}</h3>
-                <button onClick={this.handleClick} id="fav" className={favClass} data-id="tt0372784"><i className="fas fa-heart"></i></button>
-                <img className="film-details__poster" src={this.props.filmDetails.Poster} alt="poster" />
-                <h4 className="film-details__director">{this.props.filmDetails.Director}</h4>
-                <p className="film-details__meta">(Released {this.props.filmDetails.Year}, dur. {this.props.filmDetails.Runtime}, {this.props.filmDetails.Rated})</p>
-                <div className="film-details__rating">
-                    <p className="film-details__ratings__imdb"><strong>Rating:</strong></p>
-                    <div className="film-details__rating__bar" style={ratingsStyle}></div>
-                    <p className="film-details__rating__score">{this.props.filmDetails.imdbRating} out of 10</p>
+            <div className="film-details__wrapper">
+                <div className="film-details">
+                    <h3 className="film-details__title">{this.props.filmDetails.Title}</h3>
+                    <button onClick={this.handleClick} id="fav" className={favClass} data-id="tt0372784"><i className="fas fa-heart"></i></button>
+                    <img className="film-details__poster" src={this.props.filmDetails.Poster} alt="poster" />
+                    <h4 className="film-details__director">{this.props.filmDetails.Director}</h4>
+                    <p className="film-details__meta">(Released {this.props.filmDetails.Year}, dur. {this.props.filmDetails.Runtime}, {this.props.filmDetails.Rated})</p>
+                    <div className="film-details__rating">
+                        <p className="film-details__ratings__imdb"><strong>Rating:</strong></p>
+                        <div className="film-details__rating__bar" style={ratingsStyle}></div>
+                        <p className="film-details__rating__score">{this.props.filmDetails.imdbRating} out of 10</p>
+                    </div>
+                    <h4>Starring</h4>
+                    <p className="film-details__actors">
+                        {this.props.filmDetails.Actors}</p>
+                    <h4>Plot summary</h4>
+                    <p className="film-details__plot">{this.props.filmDetails.Plot}</p>
                 </div>
-                <h4>Starring</h4>
-                <p className="film-details__actors">
-                    {this.props.filmDetails.Actors}</p>
-                <h4>Plot summary</h4>
-                <p className="film-details__plot">{this.props.filmDetails.Plot}</p>
             </div>
         )
     }
