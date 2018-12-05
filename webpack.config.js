52f10d55f963b451fdb18c873fc8fc07ac254a87
Roland Levy <rolandjlevy@gmail.com>
2018-12-05 17:55:01 +0000

    Webpack bugs now fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 const path = require('path');
 const webpack = require('webpack');
-require('dotenv').config();
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+require('dotenv').config();
+
 module.exports = {
+  mode: process.env.NODE_ENV,
   entry: './src/index.js',
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         API_KEY: JSON.stringify(process.env.API_KEY)
       }
-    }), 
+    }),
     new HtmlWebpackPlugin({
-        template: 'src/assets/index.template.html'
+      template: path.resolve(__dirname, './src/assets/index.template.html')
     })
   ],
   output: {
@@ -27,7 +28,16 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         loader: require.resolve('babel-loader')
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
       }
+
     ]
   }
 };
