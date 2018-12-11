2f0cbee436ece94c4efc3aad031454be208867f3
Tony Batty <tonyeb87@gmail.com>
2018-12-11 21:57:38 +0000

    pagination styling

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,64 +1,73 @@
-import React from 'react';
+import React from "react";
 
 class Pagination extends React.Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.handleClick = this.handleClick.bind(this);
-        this.renderPreviousButton = this.renderPreviousButton.bind(this);
-        this.renderButtons = this.renderButtons.bind(this);
-        this.renderNextButton = this.renderNextButton.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.renderPreviousButton = this.renderPreviousButton.bind(this);
+    this.renderButtons = this.renderButtons.bind(this);
+    this.renderNextButton = this.renderNextButton.bind(this);
+  }
 
+  handleClick(event) {
+    if (event.target.id === "nextButton") {
+      this.props.receivePageNumber(this.props.currentPage + 1);
+    } else if (event.target.id === "previousButton") {
+      this.props.receivePageNumber(this.props.currentPage - 1);
     }
+  }
 
-    handleClick(event) {
+  renderPreviousButton() {
+    if (this.props.currentPage > 1) {
+      return (
+        <button
+          className="page-buttons"
+          id="previousButton"
+          onClick={this.handleClick}
+        >
 
-        if (event.target.id === "nextButton") {
-            this.props.receivePageNumber(this.props.currentPage + 1);
-        } else if (event.target.id === "previousButton") {
-            this.props.receivePageNumber(this.props.currentPage - 1);
-        }
+          <i class="fas fa-arrow-left" />
+          &nbsp;Previous
+        </button>
+      );
     }
+  }
 
-    renderPreviousButton() {
+  renderButtons() {
+    //TODO: add buttons with page numbers inbetween previous and next buttons
+    return <p> </p>;
+  }
 
-        if (this.props.currentPage > 1) {
-            return (
-                <button className="page-buttons" id="previousButton" onClick={this.handleClick}>Previous</button>
-            )
-        }
+  renderNextButton() {
+    if (this.props.currentPage != this.props.totalPages) {
+      return (
+        <button
+          className="page-buttons"
+          id="nextButton"
+          onClick={this.handleClick}
+        >
+          Next&nbsp;
+          <i class="fas fa-arrow-right" />
+        </button>
+      );
     }
+  }
 
-    renderButtons() {
-
-        //TODO: add buttons with page numbers inbetween previous and next buttons
-        return (
-            <p> </p>
-        )
-    }
-
-    renderNextButton() {
-
-        if (this.props.currentPage != this.props.totalPages) {
-            return (
-                <button className="page-buttons" id="nextButton" onClick={this.handleClick}>Next</button>
-            )
-        }
-    }
-
-    render() {
-
-        return (
-            <div className="pagination">
-                <p>Page {this.props.currentPage} of {this.props.totalPages}</p>
-                <div className="pagination__buttons">
-                    {this.renderPreviousButton()}
-                    {this.renderButtons()}
-                    {this.renderNextButton()}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="pagination">
+        <div className="pagination__text">
+          Page {this.props.currentPage} of {this.props.totalPages}
+        </div>
+        <div className="pagination__buttons">
+          {this.renderPreviousButton()}
+          {this.renderButtons()}
+          {this.renderNextButton()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Pagination;
